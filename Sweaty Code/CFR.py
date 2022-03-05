3129a99d6707d01e8080523dae6d2bed226584f0
riannang <99198113+riannang@users.noreply.github.com>
2022-03-05 22:37:53 +0000

    Update CFR.py

--- Sweaty Code/CFR.py
+++ Sweaty Code/CFR.py
@@ -2,3 +2,28 @@
 
 import pandas as pd
 import numpy as np
+
+class tictactoe: #Main Class
+
+    def __init__(self): #Initialises empty array for the board
+        self.board = []
+
+    def setBoard(self): #Sets the 3 rows into a 2-D array
+        for x in range (3):
+            row = []
+            for i in range (3):
+                row.append('-')
+            self.board.append(row)
+
+    def showBoard(self): #Visual display for array
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+    def startgame(self): #Place other other functions in here in the right order for the game
+        self.setBoard()
+        self.showBoard()
+
+t = tictactoe()
+t.startgame()
