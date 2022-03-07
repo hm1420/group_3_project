5ed8b21025e9d72f60c599b4d18aec54e75398eb
hm1420 <hm1420@ic.ac.uk>
2022-03-07 11:58:42 +0000

    Fixed flake8 errors. Mainly for myself.

--- Sweaty Code/CFR.py
+++ Sweaty Code/CFR.py
@@ -1,59 +1,55 @@
-## We put CFR Coded Algorithm here!
+# We put CFR Coded Algorithm here!
 
-import pandas as pd
-import numpy as np
+import pandas as pd  # noqa F401
+import numpy as np  # noqa F401
+import string  # noqa F401
+import random  # noqa F401
 
-import string
-import random
 
+class tictactoe:  # Main Class
 
-class tictactoe: #Main Class
-
-    def __init__(self): #Initialises new array for board
+    def __init__(self):  # Initialises new array for board
         self.board = []
 
-    def setBoard(self): #Sets the 3 rows into a 2-D array
-        for x in range (3):
+    def setBoard(self):  # Sets the 3 rows into a 2-D array
+        for x in range(3):
             row = []
-            for i in range (3):
+            for i in range(3):
                 row.append('-')
             self.board.append(row)
 
-    def showBoard(self): #Visual display for board when called
+    def showBoard(self):  # Visual display for board when called
         for row in self.board:
             for item in row:
                 print(item, end=" ")
             print()
 
-    def firstTurn(self): #Determines the 1st turn being either 'X' or 'O'
-        first=random.randint(0,1)
+    def firstTurn(self):  # Determines the 1st turn being either 'X' or 'O'
+        first = random.randint(0, 1)
         return first
 
-    def placement(self, row, col, computer): #Sets the row and column position from the computer input
+    def placement(self, row, col, computer):
+        # Sets the row and column position from the computer input
         self.board[row][col] = computer
 
+    def startgame(self):  # Call other functions in here to run the game.
 
-
-    def startgame(self): #Call other functions in here to run the game.
-        
         self.setBoard()
         print(self.board)
 
-        
         computer = 'X' if self.firstTurn() == 0 else 'O'
-        
+
         self.showBoard()
-        
-        row = random.randint(1,3)   #Random move from the computer adding a piece to the board
-        col = random.randint(1,3)
-        
+
+        row = random.randint(1, 3)  # Random move from the computer adding a
+        col = random.randint(1, 3)  # piece to the board
+
         self.placement(row - 1, col - 1, computer)
 
-        print(row,col)
+        print(row, col)
 
         print()
         self.showBoard()
-            
 
 
 t = tictactoe()
